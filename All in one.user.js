f66794323a4c79f2dd270af001f52fbd28bfdfce
Darker-drod <70256764+Darker-drod@users.noreply.github.com>
2020-08-26 12:37:48 +0530

    Add files via upload

---
+++ All in one.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         All in one
+// @namespace    http://tampermonkey.net/
+// @version      2.0
+// @description  try to take over the world!
+// @author       Darker
+// @match        https://eduapplet.com/CBT259/eLearnVideos.aspx
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
+;var myVar = setInterval(myTimer, 1);
+
+function myTimer() {document.getElementsByClassName("plyr__poster").remove();
+}
+;var ConVar = setInterval(controlsdeleter, 1);
+
+function controlsdeleter() {document.getElementsByClassName("plyr__controls").remove();
+}
